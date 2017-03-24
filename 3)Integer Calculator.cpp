7cf327ec98e1eec51ea13a80f878174ccfb50a57
fjoyshee <fj.oyshee@gmail.com>
2017-03-24 22:46:42 +0600

    Add files via upload

---
+++ 3)Integer Calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int number_1,number_2;
+cout<<"num 1: " ;
+cin>>number_1;
+cout<<endl<<"num 2: ";
+cin>>number_2;
+
+cout<<"Show the sum of two numbers";
+cout<<number_1 + number_2<<endl;
+
+cout<<"Show the difference of two numbers:";
+cout<<number_1 - number_2<<endl;
+cout<<"Show the product of two numbers:";
+cout<<number_1 * number_2<<endl;
+cout<<"Show the qutients of two numbers:";
+cout<<number_1 / number_2<<endl;
+cout<<"Show the remainders of two numbers:";
+cout<<number_1 % number_2<<endl;
+return 0;
+}
