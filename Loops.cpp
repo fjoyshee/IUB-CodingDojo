990f398b3903e370c2e1c9191c9626a1c883c66c
fjoyshee <fj.oyshee@gmail.com>
2017-05-27 13:02:58 +0600

    Update Loops.cpp

--- Loops.cpp
+++ Loops.cpp
@@ -15,4 +15,26 @@ cin>>n;
    return 0;
 }
 
-//9
+//51
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int n,i;
+    float sum=0;
+cout<<"Enter number of sum";
+cin>>n;
+ /*
+ int temporary_variable=n;
+ int counter=0;
+
+ while(temporary_variable!=0)
+    {
+        temporary_variable=temporary_variable/10;
+
+    counter++;
+}
+ cout<<"Current number"<<counter;
+}
+*/
