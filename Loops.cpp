ca5c3e88da45985fc76f2d570638f9d4bd814390
fjoyshee <fj.oyshee@gmail.com>
2017-05-27 11:04:54 +0600

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,18 @@
+//37
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int n;
+cin>>n;
+   while( n!=-1 )
+    {
+      cout << "value of n: " << n << endl;
+      n++;
+    }
+    cout<<"Sorry!you pressed -1";
+   return 0;
+}
+
+//9
