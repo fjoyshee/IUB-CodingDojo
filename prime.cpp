f582a9c4c747ac861d4c5332336bc554b41ddacc
fjoyshee <fj.oyshee@gmail.com>
2017-06-03 12:07:03 +0600

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,43 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"number:""<<endl;
+    int sum;
+    int temp=sum;
+    sum=0;
+    while(temp!=0)
+    {
+    int last_digit=temp%10;
+    sum=sum+last_digit;
+    cout<<last_digit<<" ";
+    temp=temp/10;
+    }
+
+return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+int main()
+/*{
+int no;
+cin>>no;
+bool isprime=true;
+for (int i=2;i<no;i++)
+{
+    if(no%i=0)
+    {
+        cout<<"Not Prime"<<endl;
+       isprime=false;
+       break;
+    }
+}
+ if(isprime)
+    cout<<no<<"is Prime";
+}
+*/
+{
+    cout
+}
